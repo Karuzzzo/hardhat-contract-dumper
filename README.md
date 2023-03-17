cf6925b46748e67e3501795779269e5fcd862a23
Karuzzzo <clastone@mail.ru>
2023-03-18 02:03:21 +0300

    it works

--- README.md
+++ README.md
@@ -1,24 +1,66 @@
-# Contract memory dumper
+# 📦 Ethereum Storage Dumper
 
-Project lists all non-zero storage values for first 100 slots, tries to decode them. 
+Ethereum Storage Dumper is a simple command-line tool 🛠️ that utilizes the Hardhat framework to inspect and decode the storage values of a given Ethereum contract. It allows users to quickly and easily understand the values stored in a contract's storage slots. 💾
 
-## Installation:
+## ✨ Features
 
-```bash
-yarn add --dev hardhat-contract-dumper
+- Inspect storage values from slot 0 to a specified limit 🔍
+- Decode storage values, including addresses, numbers, and strings 📚
+- Optional tracing mode to log each checked storage slot 🔬
+- Built using the Hardhat framework 🏗️
+
+## 📋 Prerequisites
+
+- [Node.js](https://nodejs.org/) (>= 12.0.0)
+- [Hardhat](https://hardhat.org/) (>= 2.0.0)
+
+## 🚀 Installation
+
+1. Install the `ethereum-storage-dumper` plugin in your Hardhat project:
 ```
-
-## Usage:
-
-Load plugin in Hardhat config:
+npm install --save-dev ethereum-storage-dumper
+```
+2. Require the plugin in your `hardhat.config.js`:
 
 ```javascript
-require('hardhat-contract-dumper');
+require("ethereum-storage-dumper");
 ```
 
-Run the included Hardhat task and supply contract address:
+or, for `hardhat.config.ts`:
 
-
-```bash
-npx hardhat contract-dump-memory --address <0xB8c77482e45F1F44dE1745F52C74426C631bDD52>
+```typescript
+import "ethereum-storage-dumper";
 ```
+
+## 🛠️ Usage
+
+To dump the storage values of a contract, use the following command:
+
+```
+
+npx hardhat sd --address CONTRACT_ADDRESS [--to SLOT_LIMIT, --raw]
+```
+
+- `CONTRACT_ADDRESS`: The Ethereum address of the contract you want to inspect. 📝
+- `SLOT_LIMIT`: The maximum storage slot to check (default: 100, and you dont need to go deeper). 🔢
+- `--raw`: (Optional) Print all storage slots as-is in hex 📈
+
+## 📖 Example
+
+To dump storage values of a contract with the address `0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48` and a slot limit of 100, run:
+
+```
+npx hardhat sd --address 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48
+```
+
+To enable raw mode, add the `--raw` flag:
+
+```
+npx hardhat sd --address 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48 --raw
+```
+
+## 📄 License
+
+This project is released under the [MIT License](LICENSE).
+
+Man, I love GPT4 generating, upgrading and fixing my stuff )
--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const DUMP_FROM = 0;
 const DUMP_TO = 100;
 
-task("contract-dump-memory", "Print all contract's memory slots") 
+task("sd", "storage dump (All slots it can find)") 
     .addParam("address", "contract\'s address")
+    .addParam("to", "Max storage slot to check", DUMP_TO, types.int)
+    .addFlag("raw", "log each checked slot", false, types.boolean)
     .setAction(async (taskArgs) => {    // remember to use correct address
         aggregatorAddr = taskArgs.address;
+        raw = taskArgs.trace;
 
-        console.log(`dumping all values from ${DUMP_FROM} to ${DUMP_TO}`);
+        console.log(`dumping all values from ${DUMP_FROM} to ${taskArgs.to}`);
         
-        for(i = DUMP_FROM; i <= DUMP_TO; i++) {
+        for(i = DUMP_FROM; i <= taskArgs.to; i++) {
             hexValue = '0x' + i.toString(16);
             const someValue =  await network.provider.send('eth_getStorageAt', 
             [aggregatorAddr, hexValue]);
 
-            [value_type, value] = try_decode(someValue);
+            [value_type, value] = try_decode(someValue, raw);
             switch (value_type) {
                 case 'none': 
                     break;
@@ -25,7 +28,10 @@ task("contract-dump-memory", "Print all contract's memory slots")
     });
 
 
-function try_decode(some256Value) {
+function try_decode(some256Value, raw) {
+    if (raw) {
+        return ['!   RAW', some256Value];
+    }
     if (some256Value.slice(2) == 0) { return ['none', 0] } 
     // First 24 elements are 0 
     if (some256Value.slice(2, 26) == 0) {
@@ -33,23 +39,58 @@ function try_decode(some256Value) {
         if (some256Value.slice(26, 30) != 0) {
             return ['address', ethers.utils.getAddress('0x' + some256Value.slice(26))];
         }
-
         possible_uint = ethers.BigNumber.from(some256Value);
         return ['uint256', possible_uint.toString()];
     }
+
+    let start_index = 0;
+    let end_index = 0;
+    console.log(some256Value);
     // Find possible gap with zeroes 
-    [start_index, end_index] = analyze_zeroes(some256Value);
+    [start_index, end_index] = ai_analyze_zeroes(some256Value);
     let start_encoded_element = some256Value.slice(2, start_index);
     let end_encoded_element = some256Value.slice(end_index);
-    // TODO try-catch
-    maybe_num = ethers.BigNumber.from("0x" + end_encoded_element).toNumber();
-    if (start_encoded_element.length == maybe_num) {
+    try {
+        maybe_num = ethers.BigNumber.from("0x" + end_encoded_element);
+    } catch (error) {
+        // Fuck it, just return unknown, deal with it
+        return ['unknown', some256Value];
+    }
+
+    if (ethers.BigNumber.from(start_encoded_element.length).eq(maybe_num)) {
         maybe_str = try_decode_string(start_encoded_element);
-        return ['string', maybe_str];
+        return ['string ', maybe_str];
     }
     return ['unknown', some256Value];
 }
 
+// It just telling me to write in typescript
+// Like in each freaking answer 
+// It just hates small simple projects apparently 
+function ai_analyze_zeroes(some256Value) {
+    let start_index = 0;
+    let end_index = 0;
+    let is_sequence = false;
+
+    for (let i = 2; i < some256Value.length; i++) {
+        if (!is_sequence && some256Value.slice(i, i + 2) === "00") {
+            start_index = i;
+            is_sequence = true;
+        }
+
+        if (is_sequence && some256Value[i] !== "0") {
+            end_index = i;
+            break;
+        }
+    }
+
+    // If the loop ends with an ongoing sequence of zeroes, update the end_index
+    if (is_sequence && end_index === 0) {
+        end_index = some256Value.length;
+    }
+
+    return [start_index, end_index];
+}
 function analyze_zeroes(some256Value) {
     start_index = 0;
     end_index = 0;
